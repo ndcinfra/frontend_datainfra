cd3b738546be4fff9c752f38fb8ebc4dca8d8f8c
JayJay <youngtip@gmail.com>
2019-02-20 12:51:22 +0900

    .

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const checkEmail = (email) => axios.get('/api/v1.0/auth/exists/email/' + email);
 export const checkDisplayName = (displayname) => axios.get('/v1/auth/checkDisplayName/' + displayname);
 export const localRegister = ({displayname,email,password}) => axios.post('/v1/auth/register', {displayname,email,password});
-export const localLogin = ({displayname, password}) => axios.post('/v1/auth/login', {displayname, password});
+export const localLogin = ({displayname, password}) => axios.post(BACKEND_API+'/v1/auth/login', {displayname, password});
 export const checkLoginStatus = (token) => axios.get('/v1/auth/checkLogin', {headers: {Authorization: "Bearer "+token}});
 export const socialAuth = ({provider, accessToken, email, providerId, picture}) => axios.post('/v1/auth/social', {provider, accessToken, email, providerId, picture});
 export const logout = () => axios.post('/v1/auth/logout');

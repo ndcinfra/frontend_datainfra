a48a2c0570ed04394e63ac7f5b7a62dda7fb7b87
JayJay <youngtip@gmail.com>
2019-02-20 12:55:59 +0900

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const isProduction = process.env.NODE_ENV === "production";
-const BACKEND_API = "http://localhost:8080";
+const BACKEND_API = "http://localhost:3000";
 
 export { isProduction, BACKEND_API };

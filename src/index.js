d75e1ca9a966e5a4261247bcdffdc6eae6d9c9d4
JayJay <youngtip@gmail.com>
2019-03-25 17:28:51 +0900

    .

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import 'babel-polyfill';
+//import 'babel-polyfill';
+// This must be the first line in src/index.js
+import 'react-app-polyfill/ie11';
 
 import("./styles/main.scss");
 import React from "react";

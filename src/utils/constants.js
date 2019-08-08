9b5062851d8275c5138138c59c0496d9d9c50d2d
Ted Youngsoon Lee <youngtip@gmail.com>
2019-08-08 11:25:30 +0900

    .

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,8 +5,8 @@ console.log("isProduction: ", isProduction);
 var BACKEND_API = ""; 
 
 if (isProduction == true) {
-    //BACKEND_API = "http://rbt.naddic.com:3000"; // Live
-    BACKEND_API = "http://localhost:8080";
+    BACKEND_API = "http://rbt.naddic.com:3000"; // Live
+    //BACKEND_API = "http://localhost:8080";
 }else{
     BACKEND_API = "http://localhost:8080";
 }
